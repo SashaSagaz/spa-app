7a19093763ce082d682a25eb22049c1f9cc49026
SashaSagaz <sasha.sagaz22134@gmail.com>
2025-09-25 23:28:30 +0300

    Rebuild project after clearing node_modules

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: './',  // Устанавливает базовый путь для всех ресурсов на сайте
   server: {
     proxy: {
       '/api': {
         target: 'http://109.73.206.144:6969',
         changeOrigin: true,
-        // 👇 эта строчка удаляет "/api" перед отправкой на сервер
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
-})
+});
